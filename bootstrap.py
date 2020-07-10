54261d8398237271224061721e6539d0c8b58e4d
Jinkai Zhang <64018385+cmdev-jinkai@users.noreply.github.com>
2020-07-10 17:15:17 +0000

    header of BB

--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,35 @@
+'''
+Name:Bootstrap method in estimation of returns 
+Author: Jinkai Zhang
+Date: May 20, 2020
+
+Summary:
+    The method Bootstrap of Bootstraps (BB) is proposed by Fama and French (2018) 
+
+This method provides a new estimation complementary to Averaged Historical Returns Advantages: 
+    1) Realized data become less available in long-term; 
+    2) No restriction to historical data; 3) simplify the distribution
+
+Assumptions: 
+    1.Monthly return is a process of random walk; 
+    2.Long-term return is the product of monthly return; 
+    3.The central limit theorem says bootstraped LTR converge toward nomal distribution;
+
+There are two sub-methods called NED and FS: 
+    1. NED: monthly return sampled from normal distribution with the same mean and standard deviation of historical return; 
+    2. FS: monthly return sampled directly from historical return.
+
+Steps: 
+    1. sample these monthly returns with replacement to generate each horizon’s 100,000 cumulative returns 
+    2. repeat the process by 1000 times 
+    3. calculate the statistical information
+
+Reference: 
+    Fama E.F. and French, K.R., 2018. Long-horizon returns. The Review of Asset Pricing Studies, 8(2), 232-252.
+    Link: https://academic.oup.com/raps/article-abstract/8/2/232/4810768
+
+'''
+
 import pandas as pd
 import numpy as np
 import random
