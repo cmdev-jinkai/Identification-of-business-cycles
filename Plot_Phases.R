9ab3771795c616a8c2356e5df241ca811d6a1c26
Jinkai Zhang <64018385+cmdev-jinkai@users.noreply.github.com>
2020-07-15 16:39:54 +0000

    updates of plots

--- Plot_Phases.R
+++ Plot_Phases.R
@@ -5,6 +5,7 @@ Date: July 15, 2020
 '''
 install.packages("ggplot2")
 library(ggplot2)
+library(scales)
 
 setwd("GitHub/Identification-of-business-cycles")
 
@@ -20,12 +21,46 @@ MultipleInput_6Phases =  read.csv('output_csv/MultipleInput_6Phases.csv')
 #select required columns
 SingleInput_2Phases = SingleInput_2Phases[, c(2,4,5)]
 SingleInput_4Phases = SingleInput_4Phases[, c(2,4,5)]
-SingleInput_6Phases = SingleInput_6Phases[, c(2,4,5)]
+SingleInput_6Phases = SingleInput_6Phases[, c(2,4,7)]
 
 MultipleInput_2Phases = MultipleInput_2Phases[, c(2,8,9)]
 MultipleInput_4Phases = MultipleInput_4Phases[, c(2,8,9)]
-MultipleInput_6Phases = MultipleInput_6Phases[, c(2,8,9)]
+MultipleInput_6Phases = MultipleInput_6Phases[, c(2,8,11)]
 
-ggplot(SingleInput_2Phases, aes(Date, Z_Score, colour = phase)) + 
+#Single Input
+ggplot(SingleInput_2Phases, aes(as.Date(Date, format = "%Y-%m-%d"), Z_Score, colour = phase)) + 
   geom_line(aes(group = 1)) +
-  labs(title = "Phase Identification in Two using OECD input (1964-2020)")
+  labs(title = "Phase Identification in Two using OECD input (1964-2020)") +
+  scale_x_date(labels = date_format("%m-%Y")) + 
+  labs(x = "Month")
+
+ggplot(SingleInput_4Phases, aes(as.Date(Date, format = "%Y-%m-%d"), Z_Score, colour = phase)) + 
+  geom_line(aes(group = 1)) +
+  labs(title = "Phase Identification in Four using OECD input (1964-2020)") +
+  scale_x_date(labels = date_format("%m-%Y")) + 
+  labs(x = "Month")
+
+ggplot(SingleInput_6Phases, aes(as.Date(Date, format = "%Y-%m-%d"), Z_Score, colour = phase)) + 
+  geom_line(aes(group = 1)) +
+  labs(title = "Phase Identification in Six using OECD input (1964-2020)") +
+  scale_x_date(labels = date_format("%m-%Y")) + 
+  labs(x = "Month")
+
+#Multiple inputs
+ggplot(MultipleInput_2Phases, aes(as.Date(Date, format = "%Y-%m-%d"), Z_Composite, colour = phase)) + 
+  geom_line(aes(group = 1)) +
+  labs(title = "Phase Identification in Two using multiple inputs (1993-2020)") +
+  scale_x_date(labels = date_format("%m-%Y")) + 
+  labs(x = "Month")
+
+ggplot(MultipleInput_4Phases, aes(as.Date(Date, format = "%Y-%m-%d"), Z_Composite, colour = phase)) + 
+  geom_line(aes(group = 1)) +
+  labs(title = "Phase Identification in Four using multiple inputs (1993-2020)") +
+  scale_x_date(labels = date_format("%m-%Y")) + 
+  labs(x = "Month")
+
+ggplot(MultipleInput_6Phases, aes(as.Date(Date, format = "%Y-%m-%d"), Z_Composite, colour = phase)) + 
+  geom_line(aes(group = 1)) +
+  labs(title = "Phase Identification in Six using multiple inputs (1993-2020)") +
+  scale_x_date(labels = date_format("%m-%Y")) + 
+  labs(x = "Month")
